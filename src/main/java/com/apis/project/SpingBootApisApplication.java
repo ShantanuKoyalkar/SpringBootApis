3ce377b85be1d83e2604762feef842191da4797e
shantanukoyalkar <shantanukoyalkar19@gmail.com>
2023-01-25 23:52:10 +0530

    shantanu1

---
+++ src/main/java/com/apis/project/SpingBootApisApplication.java
@@ -0,0 +1,13 @@
+package com.apis.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apis/project/SpingBootApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apis.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
